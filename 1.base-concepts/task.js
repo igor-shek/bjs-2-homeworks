4762f2996757b90b5c9bd772549f30d3a7a2f9dd
Igor Shek <igorshek@rambler.ru>
2021-05-22 14:08:28 +0300

    Task 1 v.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let D = b ** 2 - 4 * a * c;
+
+  if (D === 0) {
+    arr.push((-b + Math.sqrt(D)) / 2 * a);
+    console.log(D);
+  } else if (D > 0) {
+    arr.push((-b + Math.sqrt(D)) / 2 * a);
+    arr.push((-b - Math.sqrt(D)) / 2 * a);
+    console.log(D);
+  }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
