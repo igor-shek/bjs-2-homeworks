2ea07c85512921279e72ac37d7540ba385c7ce0c
Igor Shek <igorshek@rambler.ru>
2021-05-27 22:23:05 +0300

    Inititial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,10 +1,28 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = 101;
+  let max = -101;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+      console.log(`min = ${min}`);
+    }
 
-  return { min:min, max:max, avg:avg };
+    if (arr[i] > max) {
+      max = arr[i];
+      console.log(`max = ${max}`);
+    }
+
+    sum += arr[i];
+    console.log(`sum = ${sum}`);
+  }
+
+  avg = (sum / arr.length).toFixed(2);
+
+  return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
@@ -12,7 +30,7 @@ function worker(arr) {
   let sum;
 
   // Ваш код
-
+  ß
   return sum;
 }
 
@@ -21,6 +39,6 @@ function makeWork(arrOfArr, func) {
 
   // Ваш кода
   // for ...
-  
+
   return max
 }
