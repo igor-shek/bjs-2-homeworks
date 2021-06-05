472edac7b428e79871e5ff6b81ed9d5b4b8a37fa
Igor Shek <igorshek@rambler.ru>
2021-06-05 20:49:57 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
 
+  this.gender = gender;
+
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+
+  marks.forEach((item) => this.marks.push(item));
+}
+
+Student.prototype.getAverage = function () {
+  let sum = this.marks.reduce((acc, item) => acc + item, 0);
+
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete Student.subject;
+
+  delete Student.marks;
+
+  this.excluded = reason;
+}
