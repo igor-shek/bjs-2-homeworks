c7ab00b0c82bb381a2346fa07fe4566e8f4538ff
Igor Shek <igorshek@rambler.ru>
2021-06-08 21:06:42 +0300

    Classes task started, Object pushed again

---
+++ 5.classes/task.js
@@ -0,0 +1,100 @@
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount, state = 100, type = null) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = state;
+		this.type = type;
+	}
+
+	fix() {
+		let oneAndHalf = this.state * 1.5;
+
+		if (oneAndHalf <= 0) {
+			this.state = 0;
+		} else if (oneAndHalf > 100) {
+			this.state = 100;
+		} else {
+			this.state = oneAndHalf;
+		}
+	}
+
+	set state(newState) {
+		if (newState <= 0) {
+			this.state = 0;
+		} else if (newState > 100) {
+			this.state = 100;
+		} else {
+			this._state = newState;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount, state, type = 'magazine') {
+		super(name, releaseDate, pagesCount, state);
+
+		this.type = type;
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount, state, type = 'book') {
+		super(name, releaseDate, pagesCount, state);
+
+		this.author = author;
+
+		this.type = type;
+	}
+}
+
+class NovelBook extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount, state, type = 'novel') {
+		super(name, releaseDate, pagesCount, state);
+
+		this.author = author;
+
+		this.type = type;
+	}
+}
+
+class FantasticBook extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount, state, type = 'fantastic') {
+		super(name, releaseDate, pagesCount, state);
+
+		this.author = author;
+
+		this.type = type;
+	}
+}
+
+class DetectiveBook extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount, state, type = 'detective') {
+		super(name, releaseDate, pagesCount, state);
+
+		this.author = author;
+
+		this.type = type;
+	}
+}
+
+class Library {
+	constructor(name, books = []) {
+		this.name = name;
+		this.books = books;
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+
+	}
+}
